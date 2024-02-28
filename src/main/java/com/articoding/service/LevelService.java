8f52c2cb9381756cda37e6b815c0bef4441fe54f
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2024-02-28 12:26:32 +0100

    Add todo annotation in createLevel method
    There will not be a description in levels.

--- src/main/java/com/articoding/service/LevelService.java
+++ src/main/java/com/articoding/service/LevelService.java
@@ -64,6 +64,7 @@ public class LevelService {
             level.setClassRooms(classRoomList);
         }
 
+        //Todo description se va
         level.setDescription(levelForm.getDescription());
         level.setTitle(levelForm.getTitle());
         level.setPublicLevel(levelForm.isPublicLevel());
